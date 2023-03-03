a9a18d12ecd6a639f0de0ac59cefd55c028fbbd5
nchh12 <hoanghuync12@gmail.com>
2023-03-03 14:29:15 +0700

    Create branch

--- app/src/main/java/com/lamnguyen5464/avatar3dme/MainActivity.kt
+++ app/src/main/java/com/lamnguyen5464/avatar3dme/MainActivity.kt
@@ -16,6 +16,7 @@ class MainActivity : AppCompatActivity() {
                 .commitNow()
         }
 
-        startActivity(Intent(this, FaceShoot::class.java))
+        //startActivity(Intent(this, FaceShoot::class.java))
+        startActivity(Intent(this, ModelActivity::class.java))
     }
 }
